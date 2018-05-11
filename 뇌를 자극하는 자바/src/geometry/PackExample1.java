b4ee45c340ca3d0d39c12b703c5de81704f3cfbb
Administrator <Administrator@LHW>
2018-05-11 14:19:45 +0900

    같은 패키지의 클래스를 사용

---
+++ 뇌를 자극하는 자바/src/geometry/PackExample1.java
@@ -0,0 +1,21 @@
+/**
+ * 2018. 5. 11. Dev By Lee H.W
+   geometry
+   PackExample1.java
+ */
+package geometry;
+
+/**
+ * @author Administrator
+ *
+ */
+public class PackExample1 {
+
+	public static void main(String[] args) {
+		
+		Circle circle = new Circle(5);
+		System.out.println("반지름 = " + circle.radius);
+
+	}
+
+}

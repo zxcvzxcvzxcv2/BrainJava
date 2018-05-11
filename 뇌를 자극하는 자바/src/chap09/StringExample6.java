82056c66718b38176efa9e8bdd4f9701932f7d7c
Administrator <Administrator@LHW>
2018-05-11 14:14:45 +0900

    substring

---
+++ 뇌를 자극하는 자바/src/chap09/StringExample6.java
@@ -0,0 +1,12 @@
+package chap09;
+
+public class StringExample6 {
+
+	public static void main(String[] args) {
+		String str = "뇌를 자극하는 자바";
+		System.out.println(str.substring(3));
+		System.out.println(str.substring(3, 7));
+
+	}
+
+}

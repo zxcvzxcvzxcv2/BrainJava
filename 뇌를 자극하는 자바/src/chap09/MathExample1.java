d10748b9ca12323d006f6e260f9386a211bf1606
Administrator <Administrator@LHW>
2018-05-14 14:02:40 +0900

    수학계산

---
+++ 뇌를 자극하는 자바/src/chap09/MathExample1.java
@@ -0,0 +1,23 @@
+/**
+ * 2018. 5. 14. Dev By Lee H.W
+   chap09
+   MathExample1.java
+ */
+package chap09;
+
+/**
+ * @author Administrator
+ *
+ */
+public class MathExample1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("2의 3승은 " + Math.pow(2, 3));
+		System.out.println("16의 제곱근은? " + Math.sqrt(16));
+		System.out.println("4와 7 중 큰 수는? " + Math.max(4, 7));
+		System.out.println("4와 7 중 작은 수는? " + Math.min(4, 7));
+
+	}
+
+}
